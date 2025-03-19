22bfbd9b759e51e52a65b9cfde4ae5387040365b
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2025-03-19 18:36:04 +0900

    ..

--- src/main/java/com/example/cmtProject/controller/erp/personnelMgt/personnelController.java
+++ src/main/java/com/example/cmtProject/controller/erp/personnelMgt/personnelController.java
@@ -2,6 +2,7 @@ package com.example.cmtProject.controller.erp.personnelMgt;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,9 +18,18 @@ public class personnelController {
 	public String list() {
 		return "erp/personnelMgt/emp/list";
 	}
+	/**************나의 인사카드 수정***************/
+	@GetMapping("/myEmplist")
+	public String myEmpList() {
+		return "erp/personnelMgt/emp/myEmplist";
+	}
 	
+	/**************사원 인사카드 조회***************/
 	@GetMapping("/emplist")
 	public String empList() {
+		
+		
 		return "erp/personnelMgt/emp/emplist";
 	}
+	
 }
