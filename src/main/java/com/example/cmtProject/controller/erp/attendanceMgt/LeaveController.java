205d779a2ab1eb70a015703f484f45db968e038b
shoqying <shoqying@gmail.com>
2025-03-20 19:10:18 +0900

    250320-02

---
+++ src/main/java/com/example/cmtProject/controller/erp/attendanceMgt/LeaveController.java
@@ -0,0 +1,22 @@
+package com.example.cmtProject.controller.erp.attendanceMgt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/leave")
+public class LeaveController {
+
+	
+	private static final Logger logger = LoggerFactory.getLogger(AttendanceController.class);
+
+	@GetMapping("/list")
+	public String main() {
+		return "erp/attendanceMgt/leaveList";
+	}
+	
+	
+}
