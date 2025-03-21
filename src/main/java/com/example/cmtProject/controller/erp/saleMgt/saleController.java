9458311b0e8b6eb4bcb6ff504e0ee1f2af0dcbbf
khj <natista00@naver.com>
2025-03-21 21:03:08 +0900

    차트 추가

--- src/main/java/com/example/cmtProject/controller/erp/saleMgt/saleController.java
+++ src/main/java/com/example/cmtProject/controller/erp/saleMgt/saleController.java
@@ -1,11 +1,35 @@
 package com.example.cmtProject.controller.erp.saleMgt;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/sale")
 public class saleController {
 	
+	@GetMapping("/order")
+	public String order() {
+		
+		return "erp/saleMgt/order";
+	}
 	
+	@GetMapping("/receive")
+	public String receive() {
+		
+		return "erp/saleMgt/receive";
+	}
+	
+	@GetMapping("/shipment")
+	public String shipment() {
+		
+		return "erp/saleMgt/shipment";
+	}
+	
+	@GetMapping("/chart")
+	public String chart() {
+		
+		return "erp/saleMgt/chart";
+	}
 }
