73fed5d0eb5f43c47c1c833e726db9998b9072a0
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2025-03-21 14:09:54 +0900

    tea

--- src/main/java/com/example/cmtProject/repository/erp/employees/EmployeesRepository.java
+++ src/main/java/com/example/cmtProject/repository/erp/employees/EmployeesRepository.java
@@ -2,7 +2,7 @@ package com.example.cmtProject.repository.erp.employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.cmtProject.entity.Employees;
+import com.example.cmtProject.entity.erp.employees.Employees;
 
 public interface EmployeesRepository extends JpaRepository<Employees, Long> {
 	
