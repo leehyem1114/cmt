ea8190ba0555ab8d1b6708af220df46044566a06
khj <natista00@naver.com>
2025-03-23 20:25:39 +0900

    수주 발주 출하 디자인 기본 틀

--- src/main/java/com/example/cmtProject/controller/erp/saleMgt/saleController.java
+++ src/main/java/com/example/cmtProject/controller/erp/saleMgt/saleController.java
@@ -1,19 +1,32 @@
 package com.example.cmtProject.controller.erp.saleMgt;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.cmtProject.entity.erp.salesMgt.SalesOrder;
+import com.example.cmtProject.repository.erp.saleMgt.SalesOrderRepository;
+
 
 
 @Controller
 @RequestMapping("/sales")
 public class saleController {
 	
+	@Autowired
+	private SalesOrderRepository salesOrderRepository;
+	
+	
 	@GetMapping("/soform")
-	public String salesOrderForm() {
+	public String salesOrderForm(Model model) {
 		
-		
-		
+ 		List<SalesOrder> allList = salesOrderRepository.findAll();
+ 		model.addAttribute("soModel", allList);
+ 		
 		return "erp/salesMgt/salesOrderForm";
 	}
 	
