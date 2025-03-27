b4989839cce298a4e3b58648f88849e176e1712a
khj <natista00@naver.com>
2025-03-28 02:04:06 +0900

    수주 등록에서 목록 가져오기 완료

--- src/main/java/com/example/cmtProject/repository/erp/employees/EmployeesRepository.java
+++ src/main/java/com/example/cmtProject/repository/erp/employees/EmployeesRepository.java
@@ -1,14 +1,47 @@
 package com.example.cmtProject.repository.erp.employees;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.cmtProject.entity.erp.employees.Employees;
 
+@Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Long> {
-	 Optional<Employees> findByEmpId(String empId);
+	 
+	Optional<Employees> findByEmpId(String empId);
 	
-	
-	
+	@Query("""
+	 		SELECT e FROM Employees e
+	 		WHERE e.deptNo = :deptNo
+	 		AND e.positionNo = :positionNo
+	 		""")
+	List<Employees> getEmpName(@Param("deptNo") Long deptNo,
+			 @Param("positionNo") Long positionNo);
+	 
+	/*
+	SELECT e.empName FROM Employees e
+	 		WHERE e.deptNo = :deptCode
+	 		AND e.positionNo = :postCode
+	 		 
+	Employees : entity의 Employees.java
+	empName : Employees entity의 필드명
+	deptNo : Employees entity의 필드명
+	positionNo : Employees entity의 필드명
+	=> DB의 테이블과 컬럼명이 아님!
+	*/
+	 
+	/*
+	Employees getEmpName(@Param("deptNo") Long deptNo,
+	@Param("positionNo") Long positionNo); 
+	@Param("deptNo") : deptNo는 Employees entity의 필드명
+	@Param("positionNo") : positionNo는 Employees entity의 필드명
+	 
+	Long deptNo : deptNo는 Employees entity의 필드명
+	Long positionNo : positionNo는 Employees entity의 필드명
+	*/
 }
---
+++ src/main/java/com/example/cmtProject/repository/mes/standardInfoMgt/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.example.cmtProject.repository.mes.standardInfoMgt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cmtProject.entity.mes.standardInfoMgt.Products;
+
+public interface ProductsRepository extends JpaRepository<Products, Long> {
+
+}
