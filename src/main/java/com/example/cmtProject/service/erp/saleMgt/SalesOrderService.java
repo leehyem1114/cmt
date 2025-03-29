7fa7a66e2581e89ace020e9483247a73cf0107de
khj <natista00@naver.com>
2025-03-29 12:50:04 +0900

    메인 LIST 수정

--- src/main/java/com/example/cmtProject/service/erp/saleMgt/SalesOrderService.java
+++ src/main/java/com/example/cmtProject/service/erp/saleMgt/SalesOrderService.java
@@ -1,6 +1,25 @@
 package com.example.cmtProject.service.erp.saleMgt;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cmtProject.dto.erp.saleMgt.SalesOrderDTO;
+import com.example.cmtProject.mapper.erp.saleMgt.SalesOrderMapper;
+
+@Service
 public class SalesOrderService {
 
+	@Autowired
+  	private SalesOrderMapper salesOrderMapper;
+	
+	// 수주 메인 SELECT
+	public List<SalesOrderDTO> soMainSelect(){
+		
+		List<SalesOrderDTO> soMainList = salesOrderMapper.soMainSelect();
+		
+		return soMainList;
+	}
 	
 }
