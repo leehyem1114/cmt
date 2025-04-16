391b7c51e0e8c818203db73452c8b7cde315f4a2
shoqying <shoqying@gamil.com>
2025-04-16 17:48:10 +0900

    qcm01 완료

--- src/main/java/com/example/cmtProject/controller/mes/qualityControl/QcmController.java
+++ src/main/java/com/example/cmtProject/controller/mes/qualityControl/QcmController.java
@@ -1,11 +1,21 @@
 package com.example.cmtProject.controller.mes.qualityControl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.cmtProject.dto.erp.attendanceMgt.AttendDTO;
+import com.example.cmtProject.dto.mes.qualityControl.QcmDTO;
+import com.example.cmtProject.entity.erp.employees.Employees;
+import com.example.cmtProject.entity.erp.employees.PrincipalDetails;
+import com.example.cmtProject.service.mes.qualityControl.QcmService;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,8 +23,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 public class QcmController {
 	
+	@Autowired
+	private QcmService qcmService;
+	
 	@GetMapping("quality-info")
-	public String getMethodName() {
+	public String getMethodName(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		
+		if (principalDetails == null) {
+            return "redirect:/login"; // 로그인 페이지로 리다이렉트
+        }
+    	// 유저정보
+    	Employees loginUser = principalDetails.getUser();
+		
+    	List<QcmDTO> qcmList = qcmService.getAllQcm();
+    	model.addAttribute("qcmList", qcmList);
+    	
 		return "mes/qualityControl/qcmList";
 	}
 	
--- src/main/java/com/example/cmtProject/service/mes/qualityControl/QcmService.java
+++ src/main/java/com/example/cmtProject/service/mes/qualityControl/QcmService.java
@@ -1,8 +1,22 @@
 package com.example.cmtProject.service.mes.qualityControl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.cmtProject.dto.mes.qualityControl.QcmDTO;
+import com.example.cmtProject.mapper.mes.qualityControl.QcmMapper;
 
 @Service
 public class QcmService {
+	
+	@Autowired
+	private QcmMapper qcmMapper;
+
+	// QCM 모든 정보 출력
+	public List<QcmDTO> getAllQcm() {
+		return qcmMapper.getAllQcm();
+	}
 
 }
