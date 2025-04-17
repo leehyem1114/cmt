279eabd2a6d6b6735f66f9e5bec9f1b62618d50f
inspirexxx <zoo.oov.o@gmail.com>
2025-04-17 15:32:11 +0900

    MES 생산계획 진행중

--- src/main/java/com/example/cmtProject/mapper/mes/manufacturingMgt/MfgMapper.java
+++ src/main/java/com/example/cmtProject/mapper/mes/manufacturingMgt/MfgMapper.java
@@ -1,8 +1,15 @@
 package com.example.cmtProject.mapper.mes.manufacturingMgt;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.cmtProject.dto.mes.manufacturingMgt.MfgPlanDTO;
 
 @Mapper
 public interface MfgMapper {
+
+	// 생산 계획 등록 전체 목록
+	List<MfgPlanDTO> getMfgPlanTotalList();
 
 }
