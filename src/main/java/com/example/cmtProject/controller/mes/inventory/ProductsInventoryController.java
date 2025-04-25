b5136a07eb980410af887be3c96d59dd48a1d91e
freehyeon <tpstmalsxm@gmail.com>
2025-04-25 09:08:39 +0900

    시퀀스 반영 및 PRODUCTS 화면 수정

--- src/main/java/com/example/cmtProject/controller/mes/inventory/ProductsInventoryController.java
+++ src/main/java/com/example/cmtProject/controller/mes/inventory/ProductsInventoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.cmtProject.constants.PathConstants;
 import com.example.cmtProject.mapper.mes.inventory.ProductsInventoryMapper;
-import com.example.cmtProject.service.mes.inventory.MaterialInventoryService;
+import com.example.cmtProject.service.mes.inventory.ProductslInventoryService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,6 +21,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 
 public class ProductsInventoryController {
+	
+	@Autowired
+	private ProductslInventoryService pis;
+	
+	/**
+	 * 
+	 * 완재품 재고 메인페이지
+	 * 
+	 */
+	@GetMapping("/view")
+	public String meterialInventoryGET(Model model) {
+		Map<String,Object> findMap = new HashMap<>();
+		List<Map<String,Object>> pInventoryList = pis.pInventoryList(findMap);
+		model.addAttribute("pInventoryList", pInventoryList);
+		
+		return "mes/inventory/pInventoryList";
+	}
 	
 
 }
