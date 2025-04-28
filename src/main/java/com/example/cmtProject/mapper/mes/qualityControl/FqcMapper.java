bd68bb2c6342dfbab617e83ebe08a0db3cc86532
shoqying <shoqying@gamil.com>
2025-04-28 19:52:06 +0900

    차트 출력 완

--- src/main/java/com/example/cmtProject/mapper/mes/qualityControl/FqcMapper.java
+++ src/main/java/com/example/cmtProject/mapper/mes/qualityControl/FqcMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.example.cmtProject.dto.mes.qualityControl.FqcDTO;
+import com.example.cmtProject.dto.mes.qualityControl.InspectionSummaryDTO;
 import com.example.cmtProject.dto.mes.qualityControl.QcmDTO;
 import com.example.cmtProject.entity.erp.employees.Employees;
 
@@ -37,6 +38,10 @@ public interface FqcMapper {
 		    				  @Param("weightValue") Double weightValue,
 		    				  @Param("lengthValue") Double lengthValue,
 		    				  @Param("result") String result);
+	
+	InspectionSummaryDTO getSummary();
+	
+	List<InspectionSummaryDTO> getLast7DaysSummary();
 
 //	void updateMeasuredValues(Map<String, Object> param);
 
